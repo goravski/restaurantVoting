5225ffb1ac2f4c2b90364539bd70450e78958fe9
Evgen Goravski <evgoravsky@gmail.com>
2022-08-25 17:44:01 +0300

    Added abstract Entity class for operating ID

---
+++ src/main/java/org/goravski/restaurantVoting/model/AbstractBaseEntity.java
@@ -0,0 +1,54 @@
+package org.goravski.restaurantVoting.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.goravski.restaurantVoting.util.HasId;
+import org.hibernate.Hibernate;
+import org.springframework.data.annotation.Id;
+
+
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class AbstractBaseEntity implements HasId {
+    public static final int START_SEQ = 100000;
+
+    @Id
+    @SequenceGenerator(name = "global_seq", sequenceName = "global_seq", allocationSize = 1, initialValue = START_SEQ)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq")
+    protected Integer id;
+
+    @Override
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + ":" + id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || !getClass().equals(Hibernate.getClass(o))) {
+            return false;
+        }
+        AbstractBaseEntity that = (AbstractBaseEntity) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id == null ? 0 : id;
+    }
+}
