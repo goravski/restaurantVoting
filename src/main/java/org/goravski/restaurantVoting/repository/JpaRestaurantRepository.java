3b7b89e15fc491983309f054d60093f418f1cc31
Evgen Goravski <evgoravsky@gmail.com>
2022-08-31 18:26:23 +0300

    added Restaurant repository

---
+++ src/main/java/org/goravski/restaurantVoting/repository/JpaRestaurantRepository.java
@@ -0,0 +1,20 @@
+package org.goravski.restaurantVoting.repository;
+
+import org.goravski.restaurantVoting.model.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Transactional(readOnly = true)
+public interface JpaRestaurantRepository extends JpaRepository<Restaurant, Integer> {
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Restaurant r WHERE r.id=:id")
+    int delete(@Param("id") int id);
+
+    @Query("SELECT r FROM Restaurant r JOIN FETCH r.meals WHERE r.id=?1")
+    Restaurant getWithMeals(int id);
+}
---
+++ src/main/java/org/goravski/restaurantVoting/repository/RestaurantRepositoryImpl.java
@@ -0,0 +1,45 @@
+package org.goravski.restaurantVoting.repository;
+
+import org.goravski.restaurantVoting.exception.NotSupportedException;
+import org.goravski.restaurantVoting.model.Restaurant;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class RestaurantRepositoryImpl implements AbstractRepository<Restaurant> {
+    private final JpaRestaurantRepository repository;
+
+    public RestaurantRepositoryImpl(JpaRestaurantRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Restaurant save(Restaurant restaurant) {
+        return repository.save(restaurant);
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return repository.delete(id) != 0;
+    }
+
+    @Override
+    public Restaurant get(int id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Restaurant getByEmail(String email) throws NotSupportedException{
+        throw new NotSupportedException("This method doesn't supported");
+    }
+
+    @Override
+    public List<Restaurant> getAll() {
+        return repository.findAll();
+    }
+
+    public Restaurant getWithMeals(int id){
+        return repository.getWithMeals(id);
+    }
+}
