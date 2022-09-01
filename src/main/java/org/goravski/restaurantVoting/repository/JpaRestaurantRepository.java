49862b94299ccb54065ecfaf58fd6fb843eaf0f7
Evgen Goravski <evgoravsky@gmail.com>
2022-09-01 16:25:58 +0300

    Refactored query for getWithMeals

--- src/main/java/org/goravski/restaurantVoting/repository/JpaRestaurantRepository.java
+++ src/main/java/org/goravski/restaurantVoting/repository/JpaRestaurantRepository.java
@@ -1,6 +1,7 @@
 package org.goravski.restaurantVoting.repository;
 
 import org.goravski.restaurantVoting.model.Restaurant;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -15,6 +16,7 @@ public interface JpaRestaurantRepository extends JpaRepository<Restaurant, Integ
     @Query("DELETE FROM Restaurant r WHERE r.id=:id")
     int delete(@Param("id") int id);
 
-    @Query("SELECT r FROM Restaurant r JOIN FETCH r.meals WHERE r.id=?1")
+    @EntityGraph(value = Restaurant.GRATH_WITH_MEALS)
+    @Query("SELECT r FROM Restaurant r WHERE r.id=?1")
     Restaurant getWithMeals(int id);
 }
