e5372bfaa87d5fd7b3563dbe7a01800dda4d7c09
Evgen Goravski <evgoravsky@gmail.com>
2022-09-13 17:47:34 +0300

    Added query with Entity Grath

--- src/main/java/org/goravski/restaurantVoting/model/Vote.java
+++ src/main/java/org/goravski/restaurantVoting/model/Vote.java
@@ -6,20 +6,26 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
+import static org.goravski.restaurantVoting.model.Vote.GRATH_RESTAURANT_JOIN;
+import static org.goravski.restaurantVoting.model.Vote.GRATH_USER_JOIN;
+
 @Getter
 @Setter
-@ToString
 @NoArgsConstructor
 @Entity
 @Table(name = "votes")
-public class Vote extends AbstractBaseEntity{
+@NamedEntityGraph(name = GRATH_RESTAURANT_JOIN, attributeNodes = {@NamedAttributeNode("restaurant")})
+@NamedEntityGraph(name = GRATH_USER_JOIN, attributeNodes = {@NamedAttributeNode("user")})
+public class Vote extends AbstractBaseEntity {
+    public static final String GRATH_RESTAURANT_JOIN = "RestaurantJoin";
+    public static final String GRATH_USER_JOIN = "UserJoin";
 
     @Column(name = "date_vote", nullable = false)
     @NotNull
     private LocalDateTime dateVote;
 
     @Column(name = "vote")
-    private boolean isVote;
+    private boolean isVote = true;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "restaurant_id", nullable = false)
@@ -29,14 +35,15 @@ public class Vote extends AbstractBaseEntity{
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    public Vote(Integer id, LocalDateTime dateVote, boolean isVote,  Restaurant restaurant, User user) {
+    public Vote(Integer id, LocalDateTime dateVote, boolean isVote, Restaurant restaurant, User user) {
         super(id);
         this.dateVote = dateVote;
         this.isVote = isVote;
         this.restaurant = restaurant;
         this.user = user;
     }
-    public Vote (Vote vote){
+
+    public Vote(Vote vote) {
         this(vote.id, vote.dateVote, vote.isVote, vote.restaurant, vote.user);
     }
 }
--- src/main/java/org/goravski/restaurantVoting/repository/vote/JpaVoteRepository.java
+++ src/main/java/org/goravski/restaurantVoting/repository/vote/JpaVoteRepository.java
@@ -1,6 +1,7 @@
 package org.goravski.restaurantVoting.repository.vote;
 
 import org.goravski.restaurantVoting.model.Vote;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -10,13 +11,15 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Transactional(readOnly = true)
-public interface JpaVoteRepository extends JpaRepository <Vote, Integer> {
+public interface JpaVoteRepository extends JpaRepository<Vote, Integer> {
 
     @Transactional
     @Modifying
     @Query("DELETE FROM Vote v WHERE v.id =:id AND v.user.id =:userId")
     int delete(@Param("id") int id, @Param("userId") int userId);
 
+    @Transactional
+    @EntityGraph(Vote.GRATH_USER_JOIN)
     @Query("SELECT v FROM Vote v WHERE v.user.id=:userId ORDER BY v.dateVote DESC")
     List<Vote> getAll(@Param("userId") int userId);
 }
